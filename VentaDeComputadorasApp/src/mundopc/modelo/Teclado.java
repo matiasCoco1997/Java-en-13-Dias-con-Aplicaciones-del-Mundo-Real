5c0623d09239a7b033ca9c69a8852cee68f95e95
Matias Ariel Coco <cocomatias6@gmail.com>
2023-08-15 14:07:31 -0300

    Clase 17 y 18

---
+++ VentaDeComputadorasApp/src/mundopc/modelo/Teclado.java
@@ -0,0 +1,19 @@
+package mundopc.modelo;
+
+public class Teclado extends DispositivoEntrada{
+
+    private final int idTeclado;
+    private static int contadorTeclado;
+
+    public Teclado(String tipoDeEntrada, String marca, int idTeclado) {
+        super(tipoDeEntrada, marca);
+        this.idTeclado = ++contadorTeclado;
+    }
+
+    @Override
+    public String toString() {
+        return "Teclado{" +
+                "idTeclado=" + idTeclado +
+                "} " + super.toString(); // el super invoca el metodo toString de la clase padre
+    }
+}

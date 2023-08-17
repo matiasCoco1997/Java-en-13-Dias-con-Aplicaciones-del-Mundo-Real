f542a1636a9a6b84fb7b7728f0096d738fafee54
Matias Ariel Coco <cocomatias6@gmail.com>
2023-08-17 11:13:20 -0300

    Servicio de peliculas

--- CatalogoPeliculasApp/src/Main.java
+++ CatalogoPeliculasApp/src/Main.java
@@ -1,5 +1,10 @@
+import dominio.Pelicula;
+
 public class Main {
     public static void main(String[] args) {
 
+        Pelicula peliculaUno = new Pelicula("Batman");
+
+        System.out.println(peliculaUno);
     }
 }
